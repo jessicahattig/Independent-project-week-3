b637953a75fd14eac8a5b158a9275c6db7cb8c41
jessicahattig <jessicahattig@gmail.com>
2023-03-31 11:36:56 -0700

    Working on second test.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,10 @@
 function inputNumber() {
     return 'bleep';
 }
+
+//second test, input '1' and output bleep
+function inputNumber() {
+    const inputNumber = 1;
+    const inputNumberChange = inputNumber.toString()
+    return bleep;
+}
