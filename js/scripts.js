8107ec7bdfd0084709e0da8bc10b2ecd5c785cf8
jessicahattig <jessicahattig@gmail.com>
2023-03-31 10:44:07 -0700

    Deleted incorrectlu labeled file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 //Buisness Logic Testing
 
-function userInput(number){
-    let userInput = 0
-    const addingBleep = number.split("bleep");
-    addingBleep.forEach(function(1)) {
-        userInput++
-    });
+function addString("bleep"){
+    const userInput = number;
+    const
     return userInput;
 }
